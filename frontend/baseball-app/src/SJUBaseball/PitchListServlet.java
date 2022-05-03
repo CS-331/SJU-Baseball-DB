21388d0eef31d56a49ec8c93c778faee79603c96
Patrick <phesse001@csbsju.edu>
2022-05-03 01:35:17 -0500

    Fixed merge

--- frontend/baseball-app/src/SJUBaseball/PitchListServlet.java
+++ frontend/baseball-app/src/SJUBaseball/PitchListServlet.java
@@ -21,17 +21,56 @@ public class PitchListServlet extends HttpServlet {
        
 	@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
         String opposingTeam = request.getParameter("game");
         request.setAttribute("selectedTeam", opposingTeam);
         String date = request.getParameter("gameDate");
+        request.setAttribute("selectedDate", date);
+        String inning = request.getParameter("inning");
+        request.setAttribute("inning", inning);
+        String option= request.getParameter("options");
+        request.setAttribute("option", option);
         
         GameDAO dao = new GameDAO();
+        
     	try {
-            System.out.println(opposingTeam);
-            List<Play> playList = dao.getGamePlays(opposingTeam, date);
+    		// if theres a date but no other options
+    		/*
+    		if(date != "" && option == "None" && inning == "None")
+    		{
+                List<Play> playList = dao.getGamePlays(opposingTeam, date);
+                request.setAttribute("playList", playList);
+     
+                RequestDispatcher dispatcher = request.getRequestDispatcher("plays.jsp");
+                dispatcher.forward(request, response);
+    		}
+    		
+    		// if theres a date, and an inning but no options just show that inning rather than all of them
+    		else if(date != "" && inning != "None" && option =="None")
+    		{
+    			List<Play> playList = dao.getGamePlaysFromInning(opposingTeam, date, inning);
+                request.setAttribute("playList", playList);
+                
+    			RequestDispatcher dispatcher = request.getRequestDispatcher("inningPlays.jsp");
+                dispatcher.forward(request, response);
+    		}
+    		
+    		// if there's option selected at all, disregard all other input other than game and date
+    		else if(option != "None" )
+    		{
+
+    		}
+    		
+    		// otherise there is no date so don't go to any new page
+    		else
+    		{
+    			
+    		}
+    		*/
+    		List<Play> playList = dao.getGamePlaysFromInning(opposingTeam, date, inning);
             request.setAttribute("playList", playList);
- 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("Pitches.jsp");
+            
+			RequestDispatcher dispatcher = request.getRequestDispatcher("inningPlays.jsp");
             dispatcher.forward(request, response);
  
         } catch (SQLException e) {
