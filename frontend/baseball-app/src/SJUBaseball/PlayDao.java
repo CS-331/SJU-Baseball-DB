d41a8690a315114394136ab6fd24096ae0773cfd
John E <61020582+bluebous@users.noreply.github.com>
2022-05-03 19:40:39 -0500

    Update PlayDao.java

--- frontend/baseball-app/src/SJUBaseball/PlayDao.java
+++ frontend/baseball-app/src/SJUBaseball/PlayDao.java
@@ -4,20 +4,21 @@ import java.sql.*;
 public class PlayDao {
 	public static int addPlay(Play p) {
 		int status = 0;
-		String queryString = "Insert into play values(?,?,?,?,?,?,?,?)";
+		String queryString = "Insert into play values(?,?,?,?,?,?,?,?,?)";
 		
 		try {
 			Connection myConnection = ConnectionProvider.getCon();
 			PreparedStatement preparedStmt = myConnection.prepareStatement(queryString);
 			preparedStmt.clearParameters();
 			preparedStmt.setInt(1,p.getPitcherId());
-			preparedStmt.setInt(2,p.getGameId());
-			preparedStmt.setInt(3,p.getInningId());
-			preparedStmt.setString(4,p.getPitchType());
-			preparedStmt.setInt(5,p.getStrike());
-			preparedStmt.setFloat(6,p.getSpeed());
-			preparedStmt.setInt(7,p.getPitchCount());
-			preparedStmt.setString(8,p.getPlayResult());
+			preparedStmt.setString(2,p.getOpposingTeam());
+			preparedStmt.setString(3,p.getDate());
+			preparedStmt.setInt(4,p.getInningId());
+			preparedStmt.setString(5,p.getPitchType());
+			preparedStmt.setInt(6,p.getStrike());
+			preparedStmt.setFloat(7,p.getSpeed());
+			preparedStmt.setInt(8,p.getPitchCount());
+			preparedStmt.setString(9,p.getPlayResult());
 			status = preparedStmt.executeUpdate();
 			preparedStmt.close();
 		} catch (SQLException e) {
